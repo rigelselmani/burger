9cc2b5edb2709543776a7255469f955647325049
rigel <43711248+rigelselmani@users.noreply.github.com>
2019-04-02 17:11:39 -0400

    changes

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ else {
         host: "localhost",
         port: 3306,    
         user: "root",
-        password: "passwprd",
+        password: "password",
         database: "burgers_db"
     });
 }
